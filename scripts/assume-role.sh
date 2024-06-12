c9f52bf6f5a20ddcb7e5275ca27af8587576903d
Brian Huang <brianhhq@users.noreply.github.com>
2024-06-12 07:38:11 +0000

    update

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -2,10 +2,7 @@
 #!/bin/bash
 
 # Usage:
-# 1) ./aws-assume-role.sh <deployment_account> <deployment_role>
-
-# Download and Save
-# curl -O https://raw.githubusercontent.com/kiki-miumiu/sam-boilerplate/main/scripts/assume-role.sh
+# 1) . ./aws-assume-role.sh <deployment_account> <deployment_role>
 
 assume_role() {
     credentials=$(aws sts assume-role --role-arn arn:aws:iam::${1}:role/${2} --duration-seconds 3600 --role-session-name ${1}-${2}-$(date "+%Y%m%d_%H%M%S"))
@@ -14,8 +11,10 @@ assume_role() {
         export AWS_SECRET_ACCESS_KEY=$(echo ${credentials} | jq -r .Credentials.SecretAccessKey)
         export AWS_SESSION_TOKEN=$(echo ${credentials} | jq -r .Credentials.SessionToken)
         echo "Assume Role Successful! Account: '${1}' Role: '${2}'"
+        aws sts get-caller-identity | cat
     fi
 }
 
 if [ ! -z "$1" ] && [ ! -z "$2" ]; then
     assume_role "$1" "$2"
+fi
